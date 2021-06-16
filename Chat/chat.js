b0a43213405bfab41694c683899377b2134a519b
Dolmatova Ksenia <a@a.com>
2021-06-16 16:52:52 +0300

    16.06.2021

--- Chat/chat.js
+++ Chat/chat.js
@@ -1,40 +1,39 @@
 const http = require("http");
 const fs = require('fs').promises;
 
-// requests handle
-let messages = [];
+const host = 'localhost';
+const port = 8000;
 
 const requestListener = function (req, res) {
-  if (req.url === "/") {
-    fs.readFile(`${__dirname}/index.html`)
-      .then((html_content) => {
-        res.setHeader("Content-Type", "text/html");
-        res.writeHead(200);
-        res.end(html_content);
-        console.log(
-          `Request: ${req.method}, ${req.url}.`
-        );
 
-      });
-  } else if (req.url === "/msg" && req.method === "POST") {
-    let data = "";
-    req.once('data', chunk => {
-      data += chunk;
-    })
-    req.once('end', () => {
-      let new_msg = JSON.parse(data);
-      messages.push(new_msg);
-      console.log(`New msg added - { name: "${new_msg.name}", message: "${new_msg.message}" }.`);
-      res.end();
-    })
-  } else if (req.url === "/msg" && req.method === "GET") {
-    res.setHeader("Content-Type", "text");
-    res.writeHead(200);
-    res.end(JSON.stringify(messages));
-  }
+    let fileName;
+    let contentType;
+
+    if (req.url === "/") {
+        fileName = "index.html";
+        contentType = "text/html";
+    }
+    else if (req.url.endsWith(".css")) {
+        fileName = req.url.substr(1);
+        contentType = "text/css";
+    } else {
+        res.writeHead(500);
+        res.end("Error, unsupported");
+        return;
+    }
+
+    fs.readFile(`${__dirname}/${fileName}`)
+        .then(contents => {
+            res.setHeader("Content-Type", contentType);
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err.message);
+            return;
+        });
 };
 
-// creating server
-const port = 8000;
 const server = http.createServer(requestListener);
 server.listen(port);
