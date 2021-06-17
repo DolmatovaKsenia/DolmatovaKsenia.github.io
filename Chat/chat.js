22ef93b727a47fd35622cef1e436446b5d387d3b
Dolmatova Ksenia <a@a.com>
2021-06-17 12:28:54 +0300

    works

--- Chat/chat.js
+++ Chat/chat.js
@@ -3,37 +3,53 @@ const fs = require('fs').promises;
 
 const host = 'localhost';
 const port = 8000;
+let messages = [];
 
 const requestListener = function (req, res) {
+  if (req.url === "/") {
+    fs.readFile(`${__dirname}/index.html`)
+      .then((html_content) => {
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(200);
+        res.end(html_content);
+        console.log(
+          `Request: ${req.method}, ${req.url}.`
+        );
 
-    let fileName;
-    let contentType;
+      });
+  } else if (req.url.endsWith(".css")) {
+    fs.readFile(`${__dirname}/style.css`)
+      .then((css_content) => {
+        res.setHeader("Content-Type", "text/css");
+        res.writeHead(200);
+        res.end(css_content);
+        console.log(
+          `Request: ${req.method}, ${req.url}.`
+        );
 
-    if (req.url === "/") {
-        fileName = "index.html";
-        contentType = "text/html";
-    }
-    else if (req.url.endsWith(".css")) {
-        fileName = req.url.substr(1);
-        contentType = "text/css";
-    } else {
-        res.writeHead(500);
-        res.end("Error, this type is unsupported");
-        return;
-    }
-
-    fs.readFile(`${__dirname}/${fileName}`)
-        .then(contents => {
-            res.setHeader("Content-Type", contentType);
-            res.writeHead(200);
-            res.end(contents);
-        })
-        .catch(err => {
-            res.writeHead(500);
-            res.end(err.message);
-            return;
-        });
+      });
+  } else if (req.url === "/msg" && req.method === "POST") {
+    let data = "";
+    req.on('data', chunk => {
+      data += chunk;
+    })
+    req.on('end', () => {
+      let new_msg = JSON.parse(data);
+      messages.push(new_msg);
+      console.log(`New msg added - { name: "${new_msg.name}", message: "${new_msg.message}" }.`);
+      res.end();
+    })
+  } else if (req.url === "/msg" && req.method === "GET") {
+    res.setHeader("Content-Type", "text");
+    res.writeHead(200);
+    res.end(JSON.stringify(messages));
+  } else {
+    res.writeHead(404);
+    res.end("Not found");
+    return;
+  }
 };
+
 
 const server = http.createServer(requestListener);
 server.listen(port);
