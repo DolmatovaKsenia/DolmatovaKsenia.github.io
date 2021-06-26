e4ff3ce20f518029fc077899ad715a1cf792f8e3
DolmatovaKsenia <fantastic.drogon@gmail.com>
2021-06-26 08:09:02 +0300

    added

--- final_project/src/index.js
+++ final_project/src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '../public/index.html';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+$("#submit").click(function() {
+  let name = $("#name").val();
+  let password = $("#password").val();
+  let password2 = $("#password2").val();
+
+  if (name == "") {
+    alert("Введите имя пользователя");
+  }
+  else if (password == "") {
+    alert("Введите пароль");
+  }
+  else if (password2 == "") {
+    alert("Повторите пароль");
+  }
+  else if (password != password2) {
+    alert("Пароли не совпадают");
+  }
+  else if (password.length < 6) {
+    alert("Слишком короткий пароль");
+  } else {
+    alert("Имя" + name +"\n" + "Пароль" + password);
+  }
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
